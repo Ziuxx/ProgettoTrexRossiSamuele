0e9284534c4b0b3d86cad4c7ee4d3712f5522195
samur <samur@COMPUTER-DI-SAM>
2025-01-11 15:12:22 +0100

    Aggiungere i file di progetto.

---
+++ ProgettoTrexRossiSamuele/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ProgettoTrexRossiSamuele
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            trex = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            txtScore = new Label();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trex).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Black;
+            pictureBox1.Location = new Point(-8, 406);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(815, 50);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // trex
+            // 
+            trex.Image = Properties.Resources.running;
+            trex.Location = new Point(101, 366);
+            trex.Name = "trex";
+            trex.Size = new Size(40, 43);
+            trex.SizeMode = PictureBoxSizeMode.AutoSize;
+            trex.TabIndex = 1;
+            trex.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.obstacle_1;
+            pictureBox3.Location = new Point(463, 360);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(23, 46);
+            pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox3.TabIndex = 1;
+            pictureBox3.TabStop = false;
+            pictureBox3.Tag = "obstacle";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.obstacle_2;
+            pictureBox4.Location = new Point(656, 373);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(32, 33);
+            pictureBox4.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox4.TabIndex = 1;
+            pictureBox4.TabStop = false;
+            pictureBox4.Tag = "obstacle";
+            // 
+            // txtScore
+            // 
+            txtScore.AutoSize = true;
+            txtScore.Font = new Font("Consolas", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtScore.Location = new Point(29, 23);
+            txtScore.Name = "txtScore";
+            txtScore.Size = new Size(106, 24);
+            txtScore.TabIndex = 2;
+            txtScore.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            gameTimer.Interval = 20;
+            gameTimer.Tick += MainGameTimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtScore);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(trex);
+            Name = "Form1";
+            Tag = "obstacle";
+            Text = "T-rex game";
+            Load += Form1_Load;
+            KeyDown += keyisdown;
+            KeyUp += keyisup;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trex).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox trex;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
